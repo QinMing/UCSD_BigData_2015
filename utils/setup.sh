1429d29e1b7c22ef4daf3d76a24d03246a402417
Ming Qin <mingqin@ucsd.edu>
2015-05-11 14:00:19 -0700

    starting HW3. HW2 records are all lost

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+
 ## This file needs to be executed in the ec2 instance before launching
 ## the notebook
 ######### Settings for UCSD_BigData #######################
+
 ## Vault directory for LanchNotebookServer.py
-export EC2_VAULT='/Users/yoavfreund/BigData/Vault'
+export EC2_VAULT='/Users/qinming/iPythonNotebook/UCSD_BigData_2015/LocalScripts'
+
 ## Settings for emr uploads
-export BD_GitRoot='/Users/yoavfreund/BigData/UCSD_BigData'
-###########################################################
+export BD_GitRoot='/Users/qinming/iPythonNotebook/UCSD_BigData_2015'
